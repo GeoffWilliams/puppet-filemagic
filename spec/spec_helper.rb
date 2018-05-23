d249a6ae7411d50ed9e8aed09bfc893070830cf4
Geoff Williams <geoff@geoffwilliams.me.uk>
2018-05-23 13:33:27 +1000

    move spec/fixtures to spec/testdata - gitignored files was breaking tests

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,11 +1,11 @@
 require 'puppetlabs_spec_helper/module_spec_helper'
 TESTCASE = {
-  :appended         => File.join('spec', 'fixtures', 'testcase', 'appended.txt'),
-  :prepended        => File.join('spec', 'fixtures', 'testcase', 'prepended.txt'),
-  :unmerged         => File.join('spec', 'fixtures', 'testcase', 'unmerged.txt'),
-  :partial_append   => File.join('spec', 'fixtures', 'testcase', 'partial_append.txt'),
-  :partial_prepend  => File.join('spec', 'fixtures', 'testcase', 'partial_prepend.txt'),
-  :replaced         => File.join('spec', 'fixtures', 'testcase', 'replaced.txt'),
-  :needs_replace    => File.join('spec', 'fixtures', 'testcase', 'needs_replace.txt'),
+  :appended         => File.join('spec', 'testdata', 'appended.txt'),
+  :prepended        => File.join('spec', 'testdata', 'prepended.txt'),
+  :unmerged         => File.join('spec', 'testdata', 'unmerged.txt'),
+  :partial_append   => File.join('spec', 'testdata', 'partial_append.txt'),
+  :partial_prepend  => File.join('spec', 'testdata', 'partial_prepend.txt'),
+  :replaced         => File.join('spec', 'testdata', 'replaced.txt'),
+  :needs_replace    => File.join('spec', 'testdata', 'needs_replace.txt'),
   :data             => "Line 1\nLine 2",
 }
